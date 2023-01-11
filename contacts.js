9dbe60d3642d6c75c82e2d78cd31fb205c84c0dc
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-01-11 09:18:36 +0000

    Update contacts.js and index.js

--- contacts.js
+++ contacts.js
@@ -1,4 +1,46 @@
-// const fs = require("fs").promises;
-// const path = require("path");
+const fs = require("fs").promises;
+const path = require("path");
 
-// const contactsPath = path.basename("./contacts");
+// TODO: to analyze in detail the methods of the path module.
+// const contactsPath = path.join(__dirname, 'contacts.json');
+const contactsPath = path.join(__dirname, './db/contacts.json');
+
+console.log('contactsPath', contactsPath);
+
+// TODO: задокументировать каждую функцию
+const listContacts = async () => {
+  const dataString = await fs.readFile(contactsPath, 'utf8');
+  const data = JSON.parse(dataString);
+  return data;
+}
+
+console.log(listContacts);
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+// // TODO: задокументировать каждую функцию
+// function listContacts() {
+//   // ...твой код
+// }
+
+// function getContactById(contactId) {
+//   // ...твой код
+// }
+
+// function removeContact(contactId) {
+//   // ...твой код
+// }
+
+// function addContact(name, email, phone) {
+//   // ...твой код
+// }
