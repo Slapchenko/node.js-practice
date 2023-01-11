4f326ffab4841f9a9efb6aa8c8da3660cfa0ebd8
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-01-11 13:55:34 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 const operations = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
+program.parse(process.argv);
 
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      operations.getAllContacts();
+      break;
+
+    case "get":
+      operations.getContactById(id);
+      break;
+
+    case "add":
+      operations.addContact(name, email, phone);
+      break;
+
+    case "remove":
+      operations.removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
+
+// operations.getAllContacts().then((data) => console.log(data));
