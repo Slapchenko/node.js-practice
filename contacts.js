fa83e39e80e19493ab1ad21a8b8ad593fb7bf30d
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-01-13 14:58:09 +0000

    Update contacts.js and contacts.json

--- contacts.js
+++ contacts.js
@@ -4,25 +4,20 @@ const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
-const getAllContacts = async () => {
-  const dataString = await fs.readFile(contactsPath, "utf8");
-  const data = JSON.parse(dataString);
-
-  return data;
-};
-
 const listContacts = async () => {
-  const allContacts = await getAllContacts();
+  const data = await fs.readFile(contactsPath, "utf8");
+  const contactList = JSON.parse(data);
 
   console.log(`Сontact list: `);
-  console.table(allContacts);
+  console.table(contactList);
 
-  return allContacts ? allContacts : null;
+  return contactList ? contactList : null;
 };
 
 const getContactById = async (contactId) => {
-  const allContacts = await getAllContacts();
-  const contact = allContacts.find(({ id }) => id === contactId);
+  const data = await fs.readFile(contactsPath, "utf8");
+  const contactList = JSON.parse(data);
+  const contact = contactList.find(({ id }) => id === contactId);
 
   console.log(`Contact with ID ${contactId}: `);
   console.table(contact);
@@ -31,13 +26,14 @@ const getContactById = async (contactId) => {
 };
 
 const removeContact = async (contactId) => {
-  const allContacts = await getAllContacts();
-  const index = allContacts.findIndex(({ id }) => id === contactId);
-  const deletedContact = allContacts[index];
+  const data = await fs.readFile(contactsPath, "utf8");
+  const contactList = JSON.parse(data);
+  const index = contactList.findIndex(({ id }) => id === contactId);
+  const deletedContact = contactList[index];
 
   if (index !== -1) {
-    allContacts.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+    contactList.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contactList));
 
     console.log(
       `The contact with ID ${contactId} has been removed from the contact list: `
@@ -49,7 +45,8 @@ const removeContact = async (contactId) => {
 };
 
 const addContact = async (name, email, phone) => {
-  const allContacts = await getAllContacts();
+  const data = await fs.readFile(contactsPath, "utf8");
+  const contactList = JSON.parse(data);
   const newContact = {
     id: uuidv4(),
     name,
@@ -57,9 +54,9 @@ const addContact = async (name, email, phone) => {
     phone,
   };
 
-  allContacts.push(newContact);
+  contactList.push(newContact);
 
-  await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+  await fs.writeFile(contactsPath, JSON.stringify(contactList));
 
   console.log(
     `A contact with the name ${name} has been added to the contacts list: `
